ddfd0da7c68ffd4afed3baca978ed6c014d9ffcd
hezhongfeng <1446450047@qq.com>
2020-05-09 14:22:44 +0800

    加入了parcel，将button.js变成button.vue

---
+++ src/button.vue
@@ -0,0 +1,32 @@
+<template>
+    <button class="g-button">提交</button>
+</template>
+
+<script>
+    export default {
+        name: "button"
+    }
+</script>
+
+<style lang = "scss">
+    .g-button{
+        font-size: var(--font-size);
+        height: var(--button-height);
+        background-color: var(--button-bg);
+        color:var(--color);
+        padding: 0 1em;
+        border-radius:var(--border-radius);
+        border:1px solid var(--border-color);
+        cursor:pointer;
+        &:hover{
+            background-color: var(--button-hover-bg);
+        }
+        &:active{
+            background-color:var(--button-active-bg);
+        }
+        &:focus{
+            outline:none;
+        }
+    }
+
+</style>
