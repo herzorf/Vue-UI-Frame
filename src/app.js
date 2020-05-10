421cf805fa78f69c95e872715830aecdbd9c73e0
hezhongfeng <1446450047@qq.com>
2020-05-10 08:25:33 +0800

    创建了g-button-group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import Vue from "vue"
 import Button from "./button.vue"
 import Icon from "./icon"
-
+import ButtonGroup from "./g-button-group.vue"
 Vue.component("g-button", Button);
 Vue.component("g-icon", Icon);
+Vue.component("g-button-group",ButtonGroup)
 new Vue({
     el: "#app",
     data() {
@@ -12,6 +13,7 @@ new Vue({
             loading2: false,
             loading3: false,
             loading4: false,
+            loading5: false,
         }
     }
 })
