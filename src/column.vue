325e6fbbf927d3a322b16499ca040b8a77e7529f
hezhongfeng <1446450047@qq.com>
2020-05-17 13:22:55 +0800

    添加了align=right/left/center的属性

--- src/column.vue
+++ src/column.vue
@@ -1,44 +1,50 @@
 <template>
-    <div class="column" :class="[span && `column-${span}`, offset &&`offset-${offset}`]" :style="{paddingLeft:gutter/2 + 'px',paddingRight:gutter/2 + 'px'}">
-       <div style=" border: 1px solid red; height: 100px;"><slot></slot></div>
+    <div class="column" :class="columnClasses" :style="columnStyle">
+            <slot></slot>
     </div>
-
 </template>
 
 <script>
     export default {
-        props:{
-            span:{
-                type:[String,Number]
+        props: {
+            span: {
+                type: [String, Number]
             },
-            offset:{
-                type:[String,Number]
+            offset: {
+                type: [String, Number]
             }
         },
-        data(){
-            return{
-                gutter:0,
+        data() {
+            return {
+                gutter: 0,
             }
         },
-
+        computed: {
+            columnStyle() {
+                return{
+                    paddingLeft: this.gutter/2 + 'px',
+                    paddingRight: this.gutter/2 + 'px'
+                }
+            },
+            columnClasses(){
+                return  [this.span && `column-${this.span}`, this.offset &&`offset-${this.offset}`]
+            }
+        }
     }
 </script>
 <style lang="scss" scoped>
-    .column{
-
-
+    .column {
         width: 50%;
-
         $class: column-;
-        @for $n from 1 through 24{
-            &.#{$class}#{$n}{
-                width:($n/24)*100%;
+        @for $n from 1 through 24 {
+            &.#{$class}#{$n} {
+                width: ($n/24)*100%;
             }
         }
-        $class:offset-;
-        @for $n from 1 through 24{
-            &.#{$class}#{$n}{
-                margin-left:($n/24)*100% ;
+        $class: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class}#{$n} {
+                margin-left: ($n/24)*100%;
             }
         }
     }
--- src/row.vue
+++ src/row.vue
@@ -1,26 +1,48 @@
 <template>
-    <div class="row" :style="{marginLeft:-gutter/2 +'px',marginRight: -gutter/2 +'px'}">
+    <div class="row" :style="rowStyle" :class="rowClasses">
         <slot></slot>
     </div>
 </template>
 
 <script>
     export default {
-        props:{
-            gutter:{
-                type:[Number,String]
+        props: {
+            gutter: {
+                type: [Number, String]
+            },
+            align: {
+                type: String,
+                validator(value) {
+                    return ["left", "right", "center"].includes(value)
+                }
             }
         },
         mounted() {
-            this.$children.forEach((vm)=>{
+            this.$children.forEach((vm) => {
                 vm.gutter = this.gutter;
             })
+        },
+        computed: {
+            rowStyle() {
+                return {marginLeft: -this.gutter / 2 + 'px', marginRight: -this.gutter / 2 + 'px'}
+            },
+            rowClasses() {
+                return [this.align && `align-${this.align}`]
+            }
         }
     }
 </script>
-
 <style lang="scss" scoped>
     .row {
         display: flex;
+        &.align-left{
+            justify-content: flex-start;
+        }
+        &.align-right{
+            justify-content: flex-end;
+        }
+        &.align-center{
+            justify-content: center;
+        }
     }
 </style>
