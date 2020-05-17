c38e791c77e3c4f08baebc8b94549a7ad81a6955
hezhongfeng <1446450047@qq.com>
2020-05-17 09:39:26 +0800

    基本实现了row和column中间空格的基本功能

--- src/column.vue
+++ src/column.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="column" :class="[`column-${span}`]">
-        <slot></slot>
+    <div class="column" :class="[span && `column-${span}`, offset &&`offset-${offset}`]" :style="{paddingLeft:gutter/2 + 'px',paddingRight:gutter/2 + 'px'}">
+       <div style=" border: 1px solid red; height: 100px;"><slot></slot></div>
     </div>
 
 </template>
@@ -10,20 +10,35 @@
         props:{
             span:{
                 type:[String,Number]
+            },
+            offset:{
+                type:[String,Number]
             }
-        }
+        },
+        data(){
+            return{
+                gutter:0,
+            }
+        },
+
     }
 </script>
 <style lang="scss" scoped>
     .column{
-        height: 100px;
-        background-color: #cccccc;
+
+
         width: 50%;
-        border: 1px solid red;
+
         $class: column-;
         @for $n from 1 through 24{
             &.#{$class}#{$n}{
                 width:($n/24)*100%;
+            }
+        }
+        $class:offset-;
+        @for $n from 1 through 24{
+            &.#{$class}#{$n}{
+                margin-left:($n/24)*100% ;
             }
         }
     }
