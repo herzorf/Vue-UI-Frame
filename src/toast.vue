150572682ed3bbd1fc098e5edaa05f3f5f7b9bbe
hezhongfeng <1446450047@qq.com>
2020-05-23 15:04:04 +0800

    优化了三种动画，上下中。

--- src/toast.vue
+++ src/toast.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="toast" ref="wrapper" :class="toastClasses">
-        <slot v-if="!enableHTML"></slot>
-        <div v-else v-html="$slots.default[0]"></div>
-        <div class="line" ref="line"></div>
-        <span v-if="closeButton" @click="closeToast">{{closeButton.text}}</span>
+    <div class="container" :class="toastClasses">
+        <div class="toast" ref="wrapper">
+            <slot v-if="!enableHTML"></slot>
+            <div v-else v-html="$slots.default[0]"></div>
+            <div class="line" ref="line"></div>
+            <span v-if="closeButton" @click="closeToast">{{closeButton.text}}</span>
+        </div>
     </div>
 </template>
 <script>
@@ -30,18 +32,18 @@
                 type: Boolean,
                 default: false
             },
-            position:{
-                type:String,
-                default:"top",
-                validator(value){
-                    return ["top","bottom","middle"].indexOf(value) >=0
+            position: {
+                type: String,
+                default: "top",
+                validator(value) {
+                    return ["top", "bottom", "middle"].indexOf(value) >= 0
                 }
 
             }
         },
-        computed:{
-            toastClasses(){
-                return {[`position-${this.position}`]:true}
+        computed: {
+            toastClasses() {
+                return {[`position-${this.position}`]: true}
             }
         },
         mounted() {
@@ -85,17 +87,75 @@
     $border-radius: 4px;
     $line-height: 1.8;
     $background-color: rgb(0, 0, 0, 0.75);
-    @keyframes fadeIn {
-        0%{opacity: 0%;}
-        100%{opacity: 1;}
+    @keyframes fade-from-down {
+        0% {
+            pacity: 0%;
+            transform: translateY(100%);
+        }
+        100% {
+            opacity: 1;
+            transform: translateY(0);
+        }
     }
+    @keyframes fade-from-up {
+        0% {
+            pacity: 0%;
+            transform: translateY(-100%);
+        }
+        100% {
+            opacity: 1;
+            transform: translateY(0);
+        }
+    }
+    @keyframes fade-from-middle {
+        0% {
+            pacity: 0%;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+    .container {
+        position: fixed;
+        left: 50%;
+        transform: translateX(-50%);
+
+        &.position-top {
+            top: 0;
+
+            .toast {
+                border-top-left-radius: 0;
+                border-top-right-radius: 0;
+                animation: fade-from-up 0.5s;
+
+            }
+        }
+
+        &.position-bottom {
+            bottom: 0;
+
+            .toast {
+                border-bottom-left-radius: 0;
+                border-bottom-right-radius: 0;
+                animation: fade-from-down 0.5s;
+            }
+        }
+
+        &.position-middle {
+            top: 50%;
+            transform: translate(-50%, -50%);
+            .toast {
+                animation: fade-from-middle 0.5s;
+            }
+        }
+
+    }
+
     .toast {
-        animation:fadeIn 0.3s linear;
         display: flex;
         min-height: $min-height;
-        position: fixed;
         line-height: $line-height;
-        transform: translateX(-50%);
+        transfrm: translateX(-50%);
         font-size: $font-size;
         align-items: center;
         justify-content: center;
@@ -114,20 +174,6 @@
         & > span {
             cursor: pointer;
             flex-shrink: 0;
-        }
-        &.position-top{
-
-            left: 50%;top: 0;
-            transform: translateX(-50%);
-
-        }
-        &.position-bottom{
-            left: 50%;bottom: 0;
-            transform: translateX(-50%);
-        }
-        &.position-middle{
-            left: 50%;top: 50%;
-            transform: translate(-50%,-50%);
         }
 
     }
