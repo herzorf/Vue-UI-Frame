b51eb747902f2e4966b1940ffc48322ad5d8a459
hezhongfeng <1446450047@qq.com>
2020-05-28 11:00:30 +0800

    添加content的默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
         <div ref="contentWrapper" v-if="visible" class="content-wrapper">
             <slot name="content"></slot>
         </div>
-        <span ref="triggerWrapper">
+        <span ref="triggerWrapper" style="display: inline-block;">
             <slot></slot>
         </span>
     </div>
@@ -55,6 +55,8 @@
 </script>
 
 <style scoped lang="scss">
+    $background-color:#333;
+    $border-radius:4px;
     .popover {
         display: inline-block;
         vertical-align: middle;
@@ -62,8 +64,31 @@
     }
 
     .content-wrapper {
+        border: 1px solid $background-color;
+        border-radius: $border-radius;
+        /*box-shadow: 0 0 3px rgba(0,0,0,0.5);*/
+        filter: drop-shadow(0 1px 3px rgba(0,0,0,0.5));
+        background-color: white;
+        margin-top: -0.5em;
         position: absolute;
         transform: translateY(-100%);
-        border: 1px solid red;
+        padding: 0.5em 1em;
+        max-width: 24em;
+        word-break: break-all;
+        &::before,&::after {
+            content: '';
+            border: 10px solid transparent;
+            display: inline-block;
+            position: absolute;
+        }
+            &::before{
+                top:100%;
+                border-top-color: $background-color;
+            }
+            &::after{
+                border-top-color: white;
+                top: 99%;
+            }
+
     }
 </style>
