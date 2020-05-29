fef290d0e5beff19ee27bdb61fc72f7978936ffd
hezhongfeng <1446450047@qq.com>
2020-05-29 18:29:04 +0800

    基本完成了collapse的基本功能

---
+++ src/collapse.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="collapse">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "G-collapse",
+    }
+</script>
+
+<style scoped lang="scss">
+    $border-color: #ddd;
+    $border-radius:4px;
+    .collapse {
+        border: 1px solid  $border-color;
+        border-radius: $border-radius;
+    }
+</style>
