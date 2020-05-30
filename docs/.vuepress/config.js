f8cd52992019d8b70df82175696a11d94e86b0b3
hezhongfeng <1446450047@qq.com>
2020-05-31 07:34:57 +0800

    添加配置文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    base:"Vue-UI-Frame",
     title: 'Vue 组件库',
     description: '一个在我学习了Vue之后写的组件库，其中有按钮，文本框，网格系统，默认布局，toast，tabs，popover，手风琴',
     themeConfig: {
--- docs/components/button.md
+++ docs/components/button.md
@@ -2,8 +2,9 @@
 title:button
 ---
 # 按钮
-样式展示
+样式展示一
 
+<ClientOnly>
 <button-demo>文字</button-demo>
-
+</ClientOnly>
 使用方法
